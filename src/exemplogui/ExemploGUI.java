bcde021652fa851132af81a9029813b4fda461f4
Juan Borrajo Rodriguez <juanborrajo10@gmail.com>
2018-04-09 10:02:53 +0200

    Main

---
+++ src/exemplogui/ExemploGUI.java
@@ -0,0 +1,29 @@
+
+package exemplogui;
+
+import com.juan.ventana.Ventana;
+import com.juan.ventana.Ventana2;
+import com.juan.ventana.VentanaComponentes;
+import com.juan.ventana.VentanaComponentes2;
+import com.juan.ventana.VentanaLayout;
+import com.juan.ventana.VentanaPaneles;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class ExemploGUI {
+
+    
+    public static void main(String[] args) {
+        //Ventana prueba=new Ventana();
+       // Ventana2 prueba2=new Ventana2();
+       //VentanaComponentes a=new VentanaComponentes();
+//       VentanaLayout a=new VentanaLayout();
+//       a.distribuidorLayout();
+//      a.gridLayout();
+
+    VentanaPaneles a=new VentanaPaneles();
+    }
+
+}
